df237c5914011f0dba91d19b13a8578c7f248eea
tacterex <70931878+tacterex@users.noreply.github.com>
2023-09-25 23:13:40 +0300

    Add files via upload

---
+++ resources/app.js
@@ -0,0 +1,17 @@
+alert("Swipe on screen to hear purring")
+
+let main = document.querySelector('#main')
+
+var audio = new Audio('./resources/sound.mp3')
+audio.playbackRate = 0.7
+
+main.addEventListener('mousedown', function(){
+    console.log('Pur')
+    audio.play()
+    navigator.vibrate([200])
+})
+
+main.addEventListener('mouseup', function(){
+    audio.pause()
+    audio.currentTime = 0
+})
